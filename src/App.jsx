362a2f1373d5b23099ee73ce51b4a7afde2973f3
zhaoyi-1995 <492613075@qq.com>
2025-01-19 00:25:45 +0800

    shanchu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 
+import axios from 'axios'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+  const handleData =  async () => {
+    const res = await axios.get('https://my-project-name.zy492613075.workers.dev/')
+    console.log(JSON.stringify(res))
+  }
+
+  useEffect(() => {
+    handleData()
+  }, [])
+ 
 
   return (
     <>
